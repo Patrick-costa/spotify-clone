b3f5920bb5063e59c55fa5c77b30e7ed4d6f4abc
Patrick-costa <patrick23_costa@hotmail.com>
2022-07-20 17:38:11 -0300

    adicao service spotify

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ export const environment = {
 };
 
 export const SpotifyConfiguration = {
-  clienteId: '71e7cf8e9dbb48ed8b6656f08a52fa02',
+  clientId: '71e7cf8e9dbb48ed8b6656f08a52fa02',
   authEndpoint: 'https://accounts.spotify.com/authorize',
   redirectUrl: 'http://localhost:4200/login/',
   scopes: [
-    "user-read-currently-playing", // musica tocando agora
+    "user-read-currently-playing", // musica tocando agora.
     "user-read-recently-played", // ler musicas tocadas recentemente
     "user-read-playback-state", // ler estado do player do usuario
     "user-top-read", // top artistas e musicas do usuario
-    "user-modify-playback-state", // alterar do player do usuario
+    "user-modify-playback-state", // alterar do player do usuario.
     "user-library-read", // ler biblioteca dos usuarios
-    "playlist-read-private", // ler playlists privados
+    "playlist-read-private", // ler playlists privads
     "playlist-read-collaborative" // ler playlists colaborativas
   ]
 
